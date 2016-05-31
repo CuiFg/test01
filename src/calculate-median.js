5cdc1f71e3e30fee0e85efd5ecc04a0d9ac4e91c
CuiFg <1174910857@qq.com>
2016-05-31 21:27:48 +0800

    Update calculate-median.js

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,5 +1,36 @@
-function calculate_median(arr) {
-  //请勿改动函数名
+
+function calculate_median(arr)
+{
+	function  NumAscSort(m,n) //定义小到大排序的方法函数
+	{
+			 return m-n; 
+	}
+	
+	var arr1=[]; //定义筛选出来的数组
+	var num ;
+	for(var i=0; i<arr.length; i++)
+		{
+			if((i%2) != 0) // 说明是第偶数元素，然后将此元素取出来
+			{
+				arr1.push(arr[i]);//给myarr1数组末尾添加元素
+			}
+		}
+	arr1.sort( NumAscSort());
+	
+	if(arr1.length % 2 == 0)//说明有偶数个项
+	 	{
+			var m = arr1[arr1.length/2] ;
+			var n = arr1[arr1.length/2 - 1];
+			num = (m+n)/2;
+				
+
+		}
+	else
+		{
+			num = arr1[(arr1.length-1)/2];
+		}
+	return num;	
 }
+calculateMiddle(arr);
 
 module.exports = calculate_median;
